0f158c7bb4293121864f11f2ba6ce009d8cfbc4c
Illia <illiadovho@gmail.com>
2023-09-05 18:05:03 +0100

    Added text recognition

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
 import cv2
+from PIL import Image
+from pytesseract import pytesseract
 
 cv2.namedWindow("preview")
 vc = cv2.VideoCapture(0)
 
-if vc.isOpened(): # try to get the first frame
+if vc.isOpened():
     rval, frame = vc.read()
 else:
     rval = False
-
+pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 while rval:
     cv2.imshow("preview", frame)
+    #cv2.imwrite("image.jpg", frame)
+    #img = Image.open("image.jpg")
+    text = pytesseract.image_to_string(frame)
+    print(text[:-1])
     rval, frame = vc.read()
     key = cv2.waitKey(20)
-    if key == 27: # exit on ESC
+    if key == 27:
         break
 
 cv2.destroyWindow("preview")
